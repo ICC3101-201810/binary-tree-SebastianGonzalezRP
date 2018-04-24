0d018b64eb35887e4a4797b8543de5064117d318
svaldivieso1 <svaldivieso1@miuandes.cl>
2018-04-24 16:00:39 -0300

    Intento de arbol

---
+++ Arbol Binario/Arbol Binario/Arbol.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol_Binario
+{
+    public class Arbol  
+    {
+        public class Nodo
+        {
+            public int valor;
+            public Nodo izquierda, derecha;
+
+            public Nodo (int i, Nodo _L, Nodo _R)
+            {
+                valor = i;
+                izquierda = _L;
+                derecha = _R;
+            }
+            void insertar (int info)
+            {
+
+            }
+
+        }
+    }
+}
+
+           
---
+++ Arbol Binario/Arbol Binario/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbol_Binario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un Numero");
+
+        }
+    }
+}
